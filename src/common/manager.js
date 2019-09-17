2647b1b6c61be971c197f9b3de9eb9f5539bae9a
hhui64 <907322015@qq.com>
2019-09-18 01:31:14 +0800

    chore: 判断环境自动加载对应环境的配置文件

--- src/common/manager.js
+++ src/common/manager.js
@@ -1,12 +1,25 @@
 import consola from 'consola'
+import configDev from '../../configs/config.dev.json'
 import config from '../../configs/config.json'
 
 class common {
   constructor() {
-    this.config = config
-    Array.prototype.unique = function() {
+    this.config = process.env.NODE_ENV === 'production' ? config : configDev
+    /**
+     * 字符串 - 去除首尾空格
+     */
+    String.prototype.trim = function () {
+      return String(this).replace(/^\s+|\s+$/g, '')
+    }
+    /**
+     * 数组 - 去除重复元素
+     */
+    Array.prototype.unique = function () {
       return this ? Array.from(new Set(this)) : []
     }
+    /**
+     * 数组 - 删除指定元素
+     */
     Array.prototype.remove = function (value) {
       return this.splice(this.indexOf(value), 1)
     }
