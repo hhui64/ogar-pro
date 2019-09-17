702befd1930febd0d0c9bd29ff6e0a58516322d5
hhui64 <907322015@qq.com>
2019-09-18 01:34:11 +0800

    refactor: 重构入口文件

--- src/index.js
+++ src/index.js
@@ -4,33 +4,43 @@ import {
 import {
   common
 } from "./common/manager"
-import config from '../configs/config.json'
 import {
   eventHandler as EventHandler
 } from './event/handler'
-import Axios from 'axios'
-import { liveWatch as LiveWatch } from './live/watch'
+import db from './db/manager'
 
-(async function () {
-  const bot = new CQWebSocket(config.client)
-  const eventHandler = new EventHandler(bot)
-  // new LiveWatch(bot).init()
-  bot.connect()
-    .on('socket.error', console.error)
-    .on('socket.connecting', (wsType) => console.log('[%s] 建立连接, 请稍后...', wsType))
-    .on('socket.connect', (wsType, sock, attempts) => console.log('[%s] 连接成功 ヽ(✿ﾟ▽ﾟ)ノ 尝试了%d次', wsType, attempts))
-    .on('socket.failed', (wsType, attempts) => console.log('[%s] 连接失败 。･ﾟ･(つд`ﾟ)･ﾟ･ 尝试了%d次', wsType, attempts))
-    // .on('api.response', (resObj) => console.log('服务器响应: %O', resObj))
-    .on('socket.close', (wsType, code, desc) => console.log('[%s] 连接关闭(%d: %s)', wsType, code, desc))
-    .on('ready', () => console.log('今天又是复读复读的一天 ｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡'))
-    .on('message', (event, context, tags) => {
-      eventHandler.onMessage(event, context, tags)
-      event.stopPropagation()
-    })
-    .on('notice', (context) => {
-      eventHandler.onNotice(context)
-    })
-    .on('request', (context) => {
-      eventHandler.onRequest(context)
-    })
-})()
+class main extends common {
+  constructor() {
+    super()
+  }
+  async init() {
+    new db().init() // 连接数据库
+    const bot = new CQWebSocket(this.config.client)
+    const eventHandler = new EventHandler(bot)
+
+    bot.connect()
+      .on('socket.error', console.error)
+      .on('socket.connecting', (wsType) => console.info('[%s] 建立连接, 请稍后...', wsType))
+      .on('socket.connect', (wsType, sock, attempts) => console.success('[%s] 连接成功 ヽ(✿ﾟ▽ﾟ)ノ 尝试了%d次', wsType, attempts))
+      .on('socket.failed', (wsType, attempts) => console.error('[%s] 连接失败 。･ﾟ･(つд`ﾟ)･ﾟ･ 尝试了%d次', wsType, attempts))
+      // .on('api.response', (resObj) => console.info('服务器响应: %O', resObj))
+      .on('socket.close', (wsType, code, desc) => console.error('[%s] 连接关闭(%d: %s)', wsType, code, desc))
+      .on('ready', () => console.success('今天又是复读复读的一天 ｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡'))
+      .on('message', (event, context, tags) => {
+        eventHandler.onMessage(event, context, tags)
+        event.stopPropagation()
+      })
+      .on('notice', (context) => {
+        eventHandler.onNotice(context)
+      })
+      .on('request', (context) => {
+        eventHandler.onRequest(context)
+      })
+  }
+}
+
+new main().init()
+
+export {
+  main
+}
