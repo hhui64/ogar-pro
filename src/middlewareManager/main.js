9f919d654a595c31e01c7882c281dc51ff9fab2d
hhui64 <907322015@qq.com>
2019-10-29 04:11:00 +0800

    style(middlewareManager): 注册中间件时每次只允许注册一个中间件

--- src/middlewareManager/main.js
+++ src/middlewareManager/main.js
@@ -16,7 +16,9 @@ export default class MiddlewareManager extends Common {
         thisMiddlewareFn(this.config[thisMiddleware] || this.defaultOptions)
         return thisMiddleware
       } catch (error) {
+        console.error()
         console.error(error)
+        console.error()
       }
     })
   }
@@ -25,8 +27,9 @@ export default class MiddlewareManager extends Common {
    * @param {String} type - 消息类型
    * @param {Function} fn - 中间件函数
    */
-  static use(...fn) {
-    middleware = middleware.concat(fn.filter(thisFn => typeof thisFn === 'function'))
+  static use(fn) {
+    if (typeof fn !== 'function') throw new TypeError('中间件类型必须是function!')
+    middleware.push(fn)
     return this
   }
   /**
@@ -48,7 +51,6 @@ export default class MiddlewareManager extends Common {
       this.next(ctx, ...args)
       return
     }
-    thisMiddleware(ctx, this.next.bind(this, ctx, ...args), ...args)
-    return thisMiddleware
+    return thisMiddleware(ctx, this.next.bind(this, ctx, ...args), ...args)
   }
 }
